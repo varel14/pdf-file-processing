96785fe2fbfa9fbd835c46b16ab587eba1d2abde
Varel Moudio <varelmoudio14@gmail.com>
2026-03-29 17:27:20 +0100

    passed to PaddleOCR for file processing

--- main.py
+++ main.py
@@ -215,38 +215,52 @@ def process_exam_files():
         break
 
 def high_res_scan_ocr(pdf_bytes):
-    try:
-      t_start = time.time()
+  ocr = PaddleOCR(
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False,
+    use_textline_orientation=False)
+
+  # Run OCR inference on a sample image 
+  result = ocr.predict(
+      input="https://paddle-model-ecology.bj.bcebos.com/paddlex/imgs/demo_image/general_ocr_002.png")
+
+  # Visualize the results and save the JSON results
+  for res in result:
+      res.print()
+
+  return "Hello", 0
+    # try:
+    #   t_start = time.time()
       
-      with fitz.open(stream=pdf_bytes, filetype="pdf") as doc:
-          page = doc[0]
-          pix = page.get_pixmap(matrix=fitz.Matrix(2, 2))
+    #   with fitz.open(stream=pdf_bytes, filetype="pdf") as doc:
+    #       page = doc[0]
+    #       pix = page.get_pixmap(matrix=fitz.Matrix(2, 2))
           
-          # Conversion Pixmap -> Array Numpy (RGB)
-          img_array = np.frombuffer(pix.samples, dtype=np.uint8).reshape((pix.h, pix.w, pix.n))
+    #       # Conversion Pixmap -> Array Numpy (RGB)
+    #       img_array = np.frombuffer(pix.samples, dtype=np.uint8).reshape((pix.h, pix.w, pix.n))
           
-          # Si c'est du RGBA (4 canaux), on convertit en RGB (3 canaux) pour Paddle
-          if pix.n == 4:
-              img_array = img_array[:, :, :3]
+    #       # Si c'est du RGBA (4 canaux), on convertit en RGB (3 canaux) pour Paddle
+    #       if pix.n == 4:
+    #           img_array = img_array[:, :, :3]
 
-          # Exécution de l'OCR
-          result = ocr_engine.predict("download.png")
+    #       # Exécution de l'OCR
+    #       result = ocr_engine.predict("download.png")
 
-          logger.info(f"Okay: {result}")
+    #       logger.info(f"Okay: {result}")
           
-          extracted_text = []
-          if result and result[0]:
-              for line in result[0]:
-                  # line[1][0] contient la chaîne de texte (line[1][1] est le score de confiance)
-                  text = line[1][0]
-                  extracted_text.append(text)
+    #       extracted_text = []
+    #       if result and result[0]:
+    #           for line in result[0]:
+    #               # line[1][0] contient la chaîne de texte (line[1][1] est le score de confiance)
+    #               text = line[1][0]
+    #               extracted_text.append(text)
           
-          raw_text = " ".join(extracted_text)
+    #       raw_text = " ".join(extracted_text)
           
-          duration = time.time() - t_start
-          return raw_text, duration
-    except Exception as e:
-      print(f"Big error: {e}")
+    #       duration = time.time() - t_start
+    #       return raw_text, duration
+    # except Exception as e:
+    #   print(f"Big error: {e}")
 
 def save_to_db(old_path, text, json_data, new_path, status):
     cursor.execute("""
