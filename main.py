750e7cbebb846ca6ed2a1b157953e4059e28cc99
Varel Moudio <varelmoudio14@gmail.com>
2026-03-29 17:15:14 +0100

    passed to PaddleOCR for file processing

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ ocr_engine = PaddleOCR(
     use_textline_orientation=True,
     use_doc_orientation_classify=False,
     use_doc_unwarping=False,
+    cpu_threads=2,
     # enable_mkldnn=True             # Accélère l'inférence sur CPU Intel/AMD
 )
 
