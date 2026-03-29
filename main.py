1352adc2cac9141a2a14f906b873c19c821c42b8
Varel Moudio <varelmoudio14@gmail.com>
2026-03-29 03:59:09 +0100

    Added Ollama llm parsing

--- main.py
+++ main.py
@@ -46,23 +46,54 @@ def process_files():
     # Conversion de l'image PIL en bytes pour EasyOCR
     img_byte_arr = BytesIO()
     images[0].save(img_byte_arr, format='JPEG')
-
-    print("Test")
     
     ocr_results = reader.readtext(img_byte_arr.getvalue(), detail=0)
     raw_text = " ".join(ocr_results)
 
     print(f"Données extraites : {raw_text}")
-    break
 
-    
     # 3. Extraction via LLM local (Ollama)
-    # extracted_data = extract_metadata_local(raw_text)
+    extracted_data = extract_metadata_local(raw_text)
     
-    # print(f"Données extraites : {json.dumps(extracted_data, indent=2, ensure_ascii=False)}")
-    
+    print(f"Données extraites : {json.dumps(extracted_data, indent=2, ensure_ascii=False)}")
+    break
     # # 4. Classification (Optionnel : déplacer le fichier)
     # classify_file_on_r2(file_key, extracted_data)
+
+def extract_metadata_local(text):
+  input_text = text[:2500] 
+  
+  prompt = f"""
+  Analyse le texte d'un examen et extrait les informations en JSON pur.
+  Champs requis :
+  - education_level: (ex: Terminale, Première, Troisièmey)
+  - exam_type: (ex: Baccalauréat, Probatoire, BEPC, Partiel, etc...)
+  - year: (ex: 2023)
+  - school_name: (nom de l'établissement ou null)
+  - discipline: (ex: Mathématiques, Physique, Histoire)
+  - serie: (Spécialité ex: Série A, C, D, TI, SES, G, ou null si non applicable)
+  - language: (Si examen de langue ou série A, précise: Espagnol, Allemand, Chinois, Italien, etc., sinon null)
+
+  Texte de l'examen :
+  {input_text}
+
+  Réponds uniquement avec le bloc JSON.
+  """
+  
+  try:
+    response = ollama.generate(model='llama3.2', prompt=prompt)
+    raw_response = response['response'].strip()
+    
+    # Nettoyage des balises markdown si le LLM en ajoute
+    if "```json" in raw_response:
+      raw_response = raw_response.split("```json")[1].split("```")[0].strip()
+    elif "```" in raw_response:
+      raw_response = raw_response.split("```")[1].strip()
+        
+    return json.loads(raw_response)
+  except Exception as e:
+    print(f"❌ Erreur lors de l'extraction par LLM : {e}")
+
 
 if __name__ == "__main__":
   process_files()
